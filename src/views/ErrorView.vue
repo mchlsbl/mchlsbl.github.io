e3c868f767b55cf8efcce343b2150248edc154f0
mchlsbl <mchlsbl@proton.me>
2023-10-23 17:45:35 +0200

    Rework 404

--- src/views/ErrorView.vue
+++ src/views/ErrorView.vue
@@ -1,6 +1,8 @@
 <template>
-	<main class="text-center">
-		<h1 class="mb-5 fw-bold display">404</h1>
-		<p class="lead">{{ $t("error.text") }}</p>
-	</main>
+    <main>
+        <h1 class="mb-5 fw-bold display">404</h1>
+        <section class="lead">
+            <p>{{ $t("error.text") }}</p>
+        </section>
+    </main>
 </template>
