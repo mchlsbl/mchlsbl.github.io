91f566099a6efa89a7f40f500297df83c21076d4
mchlsbl <92347868+mchlsbl@users.noreply.github.com>
2024-08-16 15:30:47 +0200

    Update Blog

--- src/views/BlogView.vue
+++ src/views/BlogView.vue
@@ -1,48 +1,110 @@
-<script setup lang="ts">
-import Text from "@/components/Text.vue";
-</script>
-
 <template>
   <section>
-    <Text
-      title="Blog."
-      img-url="https://images.unsplash.com/photo-1527529482837-4698179dc6ce?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-      img-location="lg:order-last"
-    >
-      Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
-      eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
-      voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
-      clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
-      amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-      nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed
-      diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
-      Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor
-      sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
-      diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-      erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
-      rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum
-      dolor sit amet. Duis autem vel eum iriure dolor in hendrerit in vulputate
-      velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis
-      at vero eros et accumsan et iusto odio dignissim qui blandit praesent
-      luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem
-      ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh
-      euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi
-      enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit
-      lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum
-      iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel
-      illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto
-      odio dignissim qui blandit praesent luptatum zzril delenit augue duis
-      dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis
-      eleifend option congue nihil imperdiet doming id quod mazim placerat facer
-      possim assum. Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
-      sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam
-      erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation
-      ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
-      Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse
-      molestie consequat, vel illum dolore eu feugiat nulla facilisis. At vero
-      eos et accusam et justo duo dolores et ea rebum. Stet clita kasd
-      gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem
-      ipsum dolor sit amet, consetetur.
-    </Text>
+    <!-- Container -->
+    <div class="mx-auto w-full max-w-7xl px-5 py-16 md:px-10 md:py-20">
+      <!-- Title -->
+      <h2 class="text-3xl font-bold md:text-5xl">
+        The latest and greatest news
+      </h2>
+      <p class="mb-8 mt-4 text-sm text-gray-500 sm:text-base md:mb-12 lg:mb-16">
+        Lorem ipsum dolor sit amet elit ut aliquam
+      </p>
+      <!-- Content -->
+      <div
+        class="mb-8 grid gap-0 sm:justify-items-stretch md:mb-12 md:grid-cols-2 md:gap-4 lg:mb-16 lg:gap-8"
+      >
+        <!-- Item -->
+        <a href="#" class="flex items-center gap-4 rounded-md p-4">
+          <img
+            src="https://firebasestorage.googleapis.com/v0/b/flowspark-1f3e0.appspot.com/o/Tailspark%20Images%2FPlaceholder%20Image.svg?alt=media&token=375a1ea3-a8b6-4d63-b975-aac8d0174074"
+            alt=""
+            class="inline-block h-36 w-36 max-w-none flex-none object-cover"
+          />
+          <div class="flex flex-col items-start py-4">
+            <div class="mb-4 rounded-md bg-gray-100 px-2 py-1.5">
+              <p class="text-sm font-semibold text-blue-600">CATEGORY NAME</p>
+            </div>
+            <p class="mb-4 text-xl font-bold">The latest news with Flowspark</p>
+            <div
+              class="flex flex-col items-start text-sm text-gray-500 lg:flex-row lg:items-center"
+            >
+              <p>Laila Bahar</p>
+              <p class="mx-2 hidden lg:block">-</p>
+              <p>6 mins read</p>
+            </div>
+          </div>
+        </a>
+        <!-- Item -->
+        <a href="#" class="flex items-center gap-4 rounded-md p-4">
+          <img
+            src="https://firebasestorage.googleapis.com/v0/b/flowspark-1f3e0.appspot.com/o/Tailspark%20Images%2FPlaceholder%20Image.svg?alt=media&token=375a1ea3-a8b6-4d63-b975-aac8d0174074"
+            alt=""
+            class="inline-block h-36 w-36 max-w-none flex-none object-cover"
+          />
+          <div class="flex flex-col items-start py-4">
+            <div class="mb-4 rounded-md bg-gray-100 px-2 py-1.5">
+              <p class="text-sm font-semibold text-blue-600">CATEGORY NAME</p>
+            </div>
+            <p class="mb-4 text-xl font-bold">The latest news with Flowspark</p>
+            <div
+              class="flex flex-col items-start text-sm text-gray-500 lg:flex-row lg:items-center"
+            >
+              <p>Laila Bahar</p>
+              <p class="mx-2 hidden lg:block">-</p>
+              <p>6 mins read</p>
+            </div>
+          </div>
+        </a>
+        <!-- Item -->
+        <a href="#" class="flex items-center gap-4 rounded-md p-4">
+          <img
+            src="https://firebasestorage.googleapis.com/v0/b/flowspark-1f3e0.appspot.com/o/Tailspark%20Images%2FPlaceholder%20Image.svg?alt=media&token=375a1ea3-a8b6-4d63-b975-aac8d0174074"
+            alt=""
+            class="inline-block h-36 w-36 max-w-none flex-none object-cover"
+          />
+          <div class="flex flex-col items-start py-4">
+            <div class="mb-4 rounded-md bg-gray-100 px-2 py-1.5">
+              <p class="text-sm font-semibold text-blue-600">CATEGORY NAME</p>
+            </div>
+            <p class="mb-4 text-xl font-bold">The latest news with Flowspark</p>
+            <div
+              class="flex flex-col items-start text-sm text-gray-500 lg:flex-row lg:items-center"
+            >
+              <p>Laila Bahar</p>
+              <p class="mx-2 hidden lg:block">-</p>
+              <p>6 mins read</p>
+            </div>
+          </div>
+        </a>
+        <!-- Item -->
+        <a href="#" class="flex items-center gap-4 rounded-md p-4">
+          <img
+            src="https://firebasestorage.googleapis.com/v0/b/flowspark-1f3e0.appspot.com/o/Tailspark%20Images%2FPlaceholder%20Image.svg?alt=media&token=375a1ea3-a8b6-4d63-b975-aac8d0174074"
+            alt=""
+            class="inline-block h-36 w-36 max-w-none flex-none object-cover"
+          />
+          <div class="flex flex-col items-start py-4">
+            <div class="mb-4 rounded-md bg-gray-100 px-2 py-1.5">
+              <p class="text-sm font-semibold text-blue-600">CATEGORY NAME</p>
+            </div>
+            <p class="mb-4 text-xl font-bold">The latest news with Flowspark</p>
+            <div
+              class="flex flex-col items-start text-sm text-gray-500 lg:flex-row lg:items-center"
+            >
+              <p>Laila Bahar</p>
+              <p class="mx-2 hidden lg:block">-</p>
+              <p>6 mins read</p>
+            </div>
+          </div>
+        </a>
+      </div>
+      <!-- Button -->
+      <a
+        href="#"
+        class="mx-auto flex w-32 rounded-md bg-black px-6 py-3 text-center font-semibold text-white"
+      >
+        View More
+      </a>
+    </div>
   </section>
 </template>
