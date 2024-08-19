b46017f67a10ddd7e8b8fd4ee207c963f2551fbd
mchlsbl <92347868+mchlsbl@users.noreply.github.com>
2024-08-19 08:59:31 +0200

    Import correct icons

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,9 @@ import router from "./router";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-import { fab } from "@fortawesome/free-brands-svg-icons";
-library.add(fab);
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+library.add(faEnvelope, faGithub);
 
 const app = createApp(App);
 
