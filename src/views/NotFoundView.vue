fbfe31bf50196e3dc392dc5b758e708911b294de
mchlsbl <92347868+mchlsbl@users.noreply.github.com>
2024-08-19 10:06:21 +0200

    Center NotFoundView

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="h-screen place-content-center bg-white">
-    <div class="text-center">
-      <h1 class="text-9xl font-black text-gray-300">404</h1>
-      <p class="mt-4 text-gray-500">Uh-oh! We can't find that page.</p>
-    </div>
+  <div
+    class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center"
+  >
+    <h1 class="text-9xl font-black text-gray-300">404</h1>
+    <p class="mt-4 text-gray-500">Uh-oh! We can't find that page.</p>
   </div>
 </template>
