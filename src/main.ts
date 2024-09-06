ca4090657d060f8781a0ea081859639fe9e323a6
mchlsbl <92347868+mchlsbl@users.noreply.github.com>
2024-09-06 13:27:33 +0200

    Refactor main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,29 +5,27 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
-import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
+import { library } from "@fortawesome/fontawesome-svg-core";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
-
-library.add(faBars, faEnvelope, faGithub);
 
 const app = createApp(App);
 
 declare module "@vue/runtime-core" {
   interface ComponentCustomProperties {
-    $githubUrl: string;
     $emailAddress: string;
+    $githubUrl: string;
   }
 }
 
 app.config.globalProperties.$emailAddress = "mchldev@proton.me";
 app.config.globalProperties.$githubUrl = "https://github.com/mchlsbl";
 
+library.add(faBars, faEnvelope, faGithub);
 app.component("font-awesome-icon", FontAwesomeIcon);
 
 app.use(router);
-
 app.mount("#app");
