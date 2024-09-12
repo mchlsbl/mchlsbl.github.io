3b91f7794342c5d24d6a69038c975fc53d825670
mchlsbl <92347868+mchlsbl@users.noreply.github.com>
2024-09-12 12:57:46 +0200

    Update NotFoundView

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,8 +1,8 @@
 <template>
   <div
-    class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center"
+    class="absolute transform top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-center"
   >
-    <h1 class="text-9xl font-black text-gray-300">404</h1>
-    <p class="mt-4 text-gray-500">Uh-oh! We can't find that page.</p>
+    <h1 class="mb-4 text-9xl font-black text-gray-300">404</h1>
+    <p class="text-color">Uh-oh! We can't find that page.</p>
   </div>
 </template>
